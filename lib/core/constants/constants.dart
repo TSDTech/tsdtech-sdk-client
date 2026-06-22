e96bbbb97b9477069aab02fc19e6fceb58189e72
Risk <henrique.turazzi@foodpass.me>
2026-06-22 11:14:44 -0300

    fix: fixed formart constants

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -10,8 +10,10 @@ class Constants {
   static Environment getStage() => stage;
 
   static String getMsUrl(String msName) => switch (stage) {
-  Environment.dev => 'https://dev-$msName-415041877599.southamerica-east1.run.app',
-  Environment.hml => 'https://hml-$msName-1041798165885.southamerica-east1.run.app',
-  Environment.prod => 'https://subaccount.tsdtech.com.br',
-};
+    Environment.dev =>
+      'https://dev-$msName-415041877599.southamerica-east1.run.app',
+    Environment.hml =>
+      'https://hml-$msName-1041798165885.southamerica-east1.run.app',
+    Environment.prod => 'https://subaccount.tsdtech.com.br',
+  };
 }
